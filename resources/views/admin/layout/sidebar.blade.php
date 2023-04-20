7883c15987be0ffdee13a96b6ccb4b2b7e176983
radouane tamouss <atamousse.red@gmail.com>
2023-04-20 06:22:31 +0000

    icons edit on sidebar!

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -9,17 +9,17 @@
 
         <ul class="sidebar-menu">
 
-            <li class="active"><a class="nav-link" href="{{route('admin_home')}}" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard" ><i class="fas fa-hand-point-right"></i> <span>Dashboard</span></a></li>
+            <li class="{{ Request::is('admin/home') ? 'active' : ''}}"><a class="nav-link" href="{{route('admin_home')}}" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Dashboard" ><i class="fas fa-chart-pie"></i> <span>Dashboard</span></a></li>
 
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i><span>Page Settings</span></a>
+            <li class="nav-item dropdown {{ Request::is('admin/home-page') ? 'active' : ''}}">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-cog"></i><span>Page Settings</span></a>
                 <ul class="dropdown-menu">
                     <li class="active"><a class="nav-link" href="{{route('admin_home_page')}}"><i class="fas fa-angle-right"></i>Home Page</a></li>
                     <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i>Terms</a></li>
                 </ul>
             </li>
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right"></i><span>Job Section</span></a>
+            <li class="nav-item dropdown {{ Request::is('admin/job-category/*') ? 'active' : ''}}">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-briefcase"></i><span>Job Section</span></a>
                 <ul class="dropdown-menu">
                     <li class="active"><a class="nav-link" href="{{route('admin_job_category')}}"><i class="fas fa-angle-right"></i>Job Category</a></li>
                     <li class=""><a class="nav-link" href=""><i class="fas fa-angle-right"></i>Job_location</a></li>
